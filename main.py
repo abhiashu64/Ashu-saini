cc8bf3a8877572eb705f1beb64c37758c6987e08
abhiashu64 <cookiesabhi0064@gmail.com>
2025-03-31 10:04:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ from pyrogram.errors.exceptions.bad_request_400 import StickerEmojiInvalid
 from pyrogram.types.messages_and_media import message
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-photologo = 'https://tinypic.host/images/2025/02/07/DeWatermark.ai_1738952933236-1.png'
+photologo = 'https://graph.org/file/2a5cb7bb0144ccfda3d37-60007d94009e55415f.jpg'
 photoyt = 'https://tinypic.host/images/2025/03/18/YouTube-Logo.wine.png'
 photocp = 'https://tinypic.host/images/2025/03/28/IMG_20250328_133126.jpg'
 
